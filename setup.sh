2577c4b6f9f22a319409aa6763ccfa79fadd5e9a
Marcos Kuchak <mkuchak@hotmail.com>
2021-09-21 14:32:53 +0000

    feat: add github cli

--- setup.sh
+++ setup.sh
@@ -21,11 +21,17 @@ mv ~/.temp ~/.zshrc;
 echo -e "\n# Plugins\nzinit light zsh-users/zsh-autosuggestions\nzinit light zsh-users/zsh-completions\nzinit light zdharma/fast-syntax-highlighting\n\n# Theme\nLS_COLORS=\$LS_COLORS:'ow=01;34:' ; export LS_COLORS\n\nSPACESHIP_PROMPT_ORDER=(\n  user          # Username section\n  dir           # Current directory section\n  host          # Hostname section\n  git           # Git section (git_branch + git_status)\n  hg            # Mercurial section (hg_branch  + hg_status)\n  exec_time     # Execution time\n  line_sep      # Line break\n  vi_mode       # Vi-mode indicator\n  jobs          # Background jobs indicator\n  exit_code     # Exit code section\n  char          # Prompt character\n)\n\nSPACESHIP_USER_SHOW='always' # Shows System user name before directory name\n\nSPACESHIP_PROMPT_ADD_NEWLINE=false\n# SPACESHIP_PROMPT_SEPARATE_LINE=false # Make the prompt span across two lines\n# SPACESHIP_DIR_TRUNC=1 # Shows only the last directory folder name\n\nSPACESHIP_CHAR_SYMBOL='>'\nSPACESHIP_CHAR_SUFFIX=' '\n\n# Hide % on start\nunsetopt PROMPT_SP\n" >>  ~/.zshrc;
 echo -e "\nexec zsh" >> ~/.bashrc;
 
+# Personal Configs
+echo -e "\n# WSL fix terminal ctrl\nbindkey -e\nbindkey '^H' backward-kill-word # Control + backspace\nbindkey ';5C' forward-word # Control + arrows\nbindkey ';5D' backward-word\n\n# Linux aliases\nalias ws='cd /home/Workspaces'\nalias dot='setopt -s glob_dots'\nalias ll='ls -lah'\nalias ssh='ssh -o ServerAliveInterval=60'\nalias ssh-reset='ssh-keygen -R'\n" >> ~/.zshrc;
+
 # Sugar Git Setup
 git clone https://github.com/mkuchak/sugar-git /root/.sugar-git;
 cd /root/.sugar-git;
 bash /root/.sugar-git/install.sh;
 git config --global core.editor "code --wait";
 
-# Personal Configs
-echo -e "\n# WSL fix terminal ctrl\nbindkey -e\nbindkey '^H' backward-kill-word # Control + backspace\nbindkey ';5C' forward-word # Control + arrows\nbindkey ';5D' backward-word\n\n# Linux aliases\nalias ws='cd /home/Workspaces'\nalias dot='setopt -s glob_dots'\nalias ll='ls -lah'\nalias ssh='ssh -o ServerAliveInterval=60'\nalias ssh-reset='ssh-keygen -R'\n" >> ~/.zshrc;
+# GitHub CLI Setup
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update -y
+apt install gh -y
